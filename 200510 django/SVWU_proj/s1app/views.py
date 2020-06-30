3a391922d1a51b609a8ccf53536067b10958c3e6
ina106 <wia1006@gmail.com>
2020-06-30 13:15:13 +0900

    rr

--- 200510 django/SVWU_proj/s1app/views.py
+++ 200510 django/SVWU_proj/s1app/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404,redirect
 from .models import Gall
 
 # Create your views here.
@@ -6,7 +6,3 @@ from .models import Gall
 def home(request):
     Gall_obj = Gall.objects
     return render(request,'home.html', {"home_key" : Gall_obj})
-
-def create(request):
-    return render(request, 'create.html')
-
